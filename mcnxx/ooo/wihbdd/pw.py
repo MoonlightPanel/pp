a3c09e9c1c1f30e0d793fd1f6f951e79bd6812c2
MoonlightPanel <haminhchiennoi@gmail.com>
2026-02-08 16:21:54 +0700

    Add password input function with validation

--- mcnxx/ooo/wihbdd/pw.py
+++ mcnxx/ooo/wihbdd/pw.py
@@ -1,1 +1,11 @@
+def password():
+ while True:
+    p = input('Nhập mật khẩu: ')
+    if p == 'pyidler-idlerhadz':
+        break
+    elif p == '':
+        print('Vui lòng nhập mật khẩu!')
+    else:
+        print('Sai mật khẩu! Vui lòng thử lại!')
 
+password()
