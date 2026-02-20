5b0e3fea70acb39ebba5001606f3950bddb87bbc
MoonlightPanel <haminhchiennoi@gmail.com>
2026-02-20 12:58:16 +0700

    Update paper.py

--- mcserver/paper.py
+++ mcserver/paper.py
@@ -1,3 +1,3 @@
 import os
 os.system("wget https://fill-data.papermc.io/v1/objects/3a2848849a7635bdaf45581f07d3d4dee6780761c5db9ad8e24b4654468719ab/paper-1.21.11-117.jar")
-os.system("mv paper-1.21.11-117.jar server.jar")
+os.system("mv paper-1.21.11-117.jar mcserver/server.jar")
